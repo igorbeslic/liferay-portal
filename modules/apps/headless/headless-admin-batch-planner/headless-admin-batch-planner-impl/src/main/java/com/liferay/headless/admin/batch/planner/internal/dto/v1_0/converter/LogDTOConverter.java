81bf837f0f39c722df317c487b4ff8bbd1ad5e9d
Matija Petanjek <matija.petanjek@liferay.com>
2021-08-16 17:18:08 +0200

    COMMERCE-6491 headless-admin-batch-planner-impl - implement Log resource

---
+++ modules/apps/headless/headless-admin-batch-planner/headless-admin-batch-planner-impl/src/main/java/com/liferay/headless/admin/batch/planner/internal/dto/v1_0/converter/LogDTOConverter.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.admin.batch.planner.internal.dto.v1_0.converter;
+
+import com.liferay.batch.planner.model.BatchPlannerLog;
+import com.liferay.headless.admin.batch.planner.dto.v1_0.Log;
+import com.liferay.portal.vulcan.dto.converter.DTOConverter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Matija Petanjek
+ */
+@Component(
+	property = "dto.class.name=com.liferay.batch.planner.model.BatchPlannerLog",
+	service = {DTOConverter.class, LogDTOConverter.class}
+)
+public class LogDTOConverter implements DTOConverter<BatchPlannerLog, Log> {
+
+	@Override
+	public String getContentType() {
+		return Log.class.getSimpleName();
+	}
+
+	@Override
+	public Log toDTO(BatchPlannerLog batchPlannerLog) throws Exception {
+		return new Log() {
+			{
+				dispatchTriggerExternalReferenceCode =
+					batchPlannerLog.getDispatchTriggerERC();
+				exportTaskExternalReferenceCode =
+					batchPlannerLog.getBatchEngineExportTaskERC();
+				id = batchPlannerLog.getBatchPlannerLogId();
+				importTaskExternalReferenceCode =
+					batchPlannerLog.getBatchEngineImportTaskERC();
+				planId = batchPlannerLog.getBatchPlannerPlanId();
+				size = batchPlannerLog.getSize();
+				status = batchPlannerLog.getStatus();
+				total = batchPlannerLog.getTotal();
+			}
+		};
+	}
+
+	public List<Log> toDTOs(List<BatchPlannerLog> batchPlannerLogs)
+		throws Exception {
+
+		List<Log> logs = new ArrayList<>();
+
+		for (BatchPlannerLog batchPlannerLog : batchPlannerLogs) {
+			logs.add(toDTO(batchPlannerLog));
+		}
+
+		return logs;
+	}
+
+}
--- modules/apps/headless/headless-admin-batch-planner/headless-admin-batch-planner-impl/src/main/java/com/liferay/headless/admin/batch/planner/internal/resource/v1_0/LogResourceImpl.java
+++ modules/apps/headless/headless-admin-batch-planner/headless-admin-batch-planner-impl/src/main/java/com/liferay/headless/admin/batch/planner/internal/resource/v1_0/LogResourceImpl.java
@@ -14,9 +14,15 @@
 
 package com.liferay.headless.admin.batch.planner.internal.resource.v1_0;
 
+import com.liferay.batch.planner.service.BatchPlannerLogService;
+import com.liferay.headless.admin.batch.planner.dto.v1_0.Log;
+import com.liferay.headless.admin.batch.planner.internal.dto.v1_0.converter.LogDTOConverter;
 import com.liferay.headless.admin.batch.planner.resource.v1_0.LogResource;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.portal.vulcan.pagination.Pagination;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -27,4 +33,22 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = LogResource.class
 )
 public class LogResourceImpl extends BaseLogResourceImpl {
+
+	public Page<Log> getPlanLogsPage(Long id, Pagination pagination)
+		throws Exception {
+
+		return Page.of(
+			_logDTOConverter.toDTOs(
+				_batchPlannerLogService.getBatchPlannerLogs(
+					id, pagination.getStartPosition(),
+					pagination.getEndPosition())),
+			pagination, _batchPlannerLogService.getBatchPlannerLogsCount(id));
+	}
+
+	@Reference
+	private BatchPlannerLogService _batchPlannerLogService;
+
+	@Reference
+	private LogDTOConverter _logDTOConverter;
+
 }
