f11d83fae20d317ef666484409da0c8e5fa9c487
mahmoud.azzam <mahmoud.azzam@liferay.com>
2022-04-25 15:13:13 +0100

    COMMERCE-9018 - batch-planner-service - buildService

--- modules/apps/batch-planner/batch-planner-service/src/main/java/com/liferay/batch/planner/service/persistence/impl/BatchPlannerPlanPersistenceImpl.java
+++ modules/apps/batch-planner/batch-planner-service/src/main/java/com/liferay/batch/planner/service/persistence/impl/BatchPlannerPlanPersistenceImpl.java
@@ -33,14 +33,20 @@ import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.dao.orm.SQLQuery;
 import com.liferay.portal.kernel.dao.orm.Session;
 import com.liferay.portal.kernel.dao.orm.SessionFactory;
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.sanitizer.Sanitizer;
+import com.liferay.portal.kernel.sanitizer.SanitizerException;
+import com.liferay.portal.kernel.sanitizer.SanitizerUtil;
 import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
+import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
 import com.liferay.portal.kernel.security.permission.InlineSQLHelperUtil;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
 import com.liferay.portal.kernel.service.persistence.BasePersistence;
 import com.liferay.portal.kernel.service.persistence.impl.BasePersistenceImpl;
+import com.liferay.portal.kernel.util.ContentTypes;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.PropsKeys;
@@ -5341,6 +5347,32 @@ public class BatchPlannerPlanPersistenceImpl
 			else {
 				batchPlannerPlan.setModifiedDate(
 					serviceContext.getModifiedDate(date));
+			}
+		}
+
+		long userId = GetterUtil.getLong(PrincipalThreadLocal.getName());
+
+		if (userId > 0) {
+			long companyId = batchPlannerPlan.getCompanyId();
+
+			long groupId = 0;
+
+			long batchPlannerPlanId = 0;
+
+			if (!isNew) {
+				batchPlannerPlanId = batchPlannerPlan.getPrimaryKey();
+			}
+
+			try {
+				batchPlannerPlan.setName(
+					SanitizerUtil.sanitize(
+						companyId, groupId, userId,
+						BatchPlannerPlan.class.getName(), batchPlannerPlanId,
+						ContentTypes.TEXT_PLAIN, Sanitizer.MODE_ALL,
+						batchPlannerPlan.getName(), null));
+			}
+			catch (SanitizerException sanitizerException) {
+				throw new SystemException(sanitizerException);
 			}
 		}
 
