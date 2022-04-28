865c96f1c12626897ec423c2b883de1ead04e36c
mahmoud.azzam <mahmoud.azzam@liferay.com>
2022-04-29 00:36:22 +0100

    COMMERCE-9018 - batch-planner-web - js - Add fileName as query parameter to the download content and failed report endpoints

--- modules/apps/batch-planner/batch-planner-web/src/main/resources/META-INF/resources/js/DownloadHelper.js
+++ modules/apps/batch-planner/batch-planner-web/src/main/resources/META-INF/resources/js/DownloadHelper.js
@@ -16,39 +16,46 @@ import {fetch} from 'frontend-js-web';
 
 import {HEADLESS_BATCH_ENGINE_URL} from './constants';
 
-const getEndpoint = (type, externalReferenceCode) => {
+const getEndpoint = (type, externalReferenceCode, fileName) => {
 	const endpoints = {
-		errorReport: `${HEADLESS_BATCH_ENGINE_URL}/import-task/by-external-reference-code/${externalReferenceCode}/failed-items/report`,
-		importFile: `${HEADLESS_BATCH_ENGINE_URL}/import-task/by-external-reference-code/${externalReferenceCode}/content`,
+		errorReport: `${HEADLESS_BATCH_ENGINE_URL}/import-task/by-external-reference-code/${externalReferenceCode}/failed-items/report?fileName=${fileName}`,
+		importFile: `${HEADLESS_BATCH_ENGINE_URL}/import-task/by-external-reference-code/${externalReferenceCode}/content?fileName=${fileName}`,
 	};
 
 	return endpoints[type];
 };
 
-export default function ({HTMLElementId, externalReferenceCode, type}) {
+export default function ({
+	HTMLElementId,
+	externalReferenceCode,
+	fileName,
+	type,
+}) {
 	document
 		.getElementById(HTMLElementId)
 		.addEventListener('click', (event) => {
 			event.preventDefault();
 
-			fetch(getEndpoint(type, externalReferenceCode)).then((response) => {
-				response.blob().then((blob) => {
-					const LinkElement = document.createElement('a');
+			fetch(getEndpoint(type, externalReferenceCode, fileName)).then(
+				(response) => {
+					response.blob().then((blob) => {
+						const LinkElement = document.createElement('a');
 
-					LinkElement.href = URL.createObjectURL(blob);
+						LinkElement.href = URL.createObjectURL(blob);
 
-					const fileName = response.headers
-						.get('Content-Disposition')
-						.match(/filename=(.*)/)[1];
+						const fileName = response.headers
+							.get('Content-Disposition')
+							.match(/filename=(.*)/)[1];
 
-					LinkElement.download = fileName;
+						LinkElement.download = fileName;
 
-					document.body.appendChild(LinkElement);
+						document.body.appendChild(LinkElement);
 
-					LinkElement.click();
+						LinkElement.click();
 
-					LinkElement.remove();
-				});
-			});
+						LinkElement.remove();
+					});
+				}
+			);
 		});
 }
